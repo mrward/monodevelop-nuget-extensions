b3acfc696e71193561f8119a5a313221b24aaccb
Matt Ward <ward.matt@gmail.com>
2014-11-23 12:27:41 +0000

    Move scriptcs and common.logging to separate project.

---
+++ src/ScriptCs/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("scriptcs")]
+
+[assembly: Guid("f624ee58-910a-4541-a46f-afcd3edca9df")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: InternalsVisibleTo("ScriptCs.Core.Tests")]
---
+++ src/ScriptCs/Properties/CommonVersionInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyInformationalVersion("0.10.2")]
