6cadc28cec2975f0183af388f68d400764d96fd8
Matt Ward <ward.matt@gmail.com>
2018-06-12 22:19:16 +0100

    Update addin version to 0.13

--- src/MonoDevelop.PackageManagement.PowerShell/Configuration/AssemblyInfo.cs
+++ src/MonoDevelop.PackageManagement.PowerShell/Configuration/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.12.6")]
+[assembly: AssemblyVersion("0.13")]
