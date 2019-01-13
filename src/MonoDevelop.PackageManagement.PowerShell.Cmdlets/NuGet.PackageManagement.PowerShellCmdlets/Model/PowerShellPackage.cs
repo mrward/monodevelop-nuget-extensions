7d71bbc039f0557e86082e859c4f323b2b813b08
Matt Ward <ward.matt@gmail.com>
2019-01-13 15:03:40 +0000

    Implement Find-Package
    Use code from NuGet.Client for Find-Package.
    Source parameter not implemented.
    Active source in package console is not used by default.

---
+++ src/MonoDevelop.PackageManagement.PowerShell.Cmdlets/NuGet.PackageManagement.PowerShellCmdlets/Model/PowerShellPackage.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
+using NuGet.Versioning;
+
+namespace NuGet.PackageManagement.PowerShellCmdlets
+{
+	/// <summary>
+	/// Represent the view of packages by Id and Versions
+	/// </summary>
+	public class PowerShellPackage
+	{
+		public string Id { get; set; }
+
+		public IEnumerable<NuGetVersion> Versions {
+			get {
+				var versions = Task.Run (async delegate {
+					var result = (await AsyncLazyVersions.GetValueAsync ()) ?? Enumerable.Empty<NuGetVersion> ();
+
+					if (result.Any ()) {
+						if (AllVersions) {
+							return result;
+						} else {
+							// result has at least 1 element
+							return result.Take (1);
+						}
+					}
+
+					return null;
+				});
+				versions.Wait ();
+				return versions.Result;
+			}
+		}
+
+		public AsyncLazy<IEnumerable<NuGetVersion>> AsyncLazyVersions { get; set; }
+
+		public NuGet.SemanticVersion Version {
+			get {
+				var nVersion = Versions.FirstOrDefault ();
+
+				if (nVersion != null) {
+					NuGet.SemanticVersion sVersion;
+					NuGet.SemanticVersion.TryParse (nVersion.ToNormalizedString (), out sVersion);
+					return sVersion;
+				}
+
+				return null;
+			}
+		}
+
+		public bool AllVersions { get; set; }
+
+		public string LicenseUrl { get; set; }
+	}
+}
