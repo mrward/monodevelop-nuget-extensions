b813cbf60ffaaa72b8cd71c92ac46aafc6c985b5
Matt Ward <ward.matt@gmail.com>
2019-01-28 22:43:44 +0000

    Refactor to remove code duplication in protocol message handling
    Added new message handlers for installing and uninstalling so member
    variables can be used and avoid duplicating the code for previewing
    the uninstall and install NuGet package actions.

---
+++ src/MonoDevelop.PackageManagement.Extensions/MonoDevelop.PackageManagement.Protocol/InstallPackageMessageHandler.cs
@@ -0,0 +1,187 @@
+﻿//
+// InstallPackageMessageHandler.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2019 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using MonoDevelop.PackageManagement.PowerShell.Protocol;
+using MonoDevelop.Projects;
+using NuGet.PackageManagement;
+using NuGet.Packaging.Core;
+using NuGet.ProjectManagement;
+using NuGet.Protocol.Core.Types;
+using NuGet.Resolver;
+using NuGet.Versioning;
+
+namespace MonoDevelop.PackageManagement.Protocol
+{
+	class InstallPackageMessageHandler
+	{
+		DotNetProject project;
+		InstallPackageParams message;
+		IMonoDevelopSolutionManager solutionManager;
+		NuGetProject nugetProject;
+		MonoDevelopNuGetPackageManager packageManager;
+		NuGetProjectContext projectContext;
+		PackageIdentity identity;
+
+		public InstallPackageMessageHandler (DotNetProject project, InstallPackageParams message)
+		{
+			this.project = project;
+			this.message = message;
+		}
+
+		public Task<IEnumerable<NuGetProjectAction>> PreviewInstallPackage (
+			CancellationToken token)
+		{
+			using (var sourceCacheContext = new SourceCacheContext ()) {
+				return PreviewInstallPackage (token, sourceCacheContext);
+			}
+		}
+
+		public async Task<IEnumerable<NuGetProjectAction>> PreviewInstallPackage (
+			CancellationToken token,
+			SourceCacheContext sourceCacheContext)
+		{
+			solutionManager = project.GetSolutionManager ();
+			nugetProject = project.CreateNuGetProject (solutionManager);
+
+			packageManager = new MonoDevelopNuGetPackageManager (solutionManager);
+			var repositories = GetSourceRepositories (message.PackageSources);
+
+			projectContext = new NuGetProjectContext (solutionManager.Settings);
+			var dependencyBehavior = (DependencyBehavior)Enum.Parse (typeof (DependencyBehavior), message.DependencyBehavior);
+
+			NuGetVersion version = null;
+			if (string.IsNullOrEmpty (message.PackageVersion)) {
+				version = await GetLatestPackageVersion (repositories, dependencyBehavior, sourceCacheContext);
+			} else {
+				version = NuGetVersion.Parse (message.PackageVersion);
+			}
+
+			identity = new PackageIdentity (message.PackageId, version);
+
+			var resolutionContext = new ResolutionContext (
+				dependencyBehavior,
+				message.AllowPrerelease,
+				true,
+				VersionConstraints.None,
+				new GatherCache (),
+				sourceCacheContext
+			);
+
+			return await packageManager.PreviewInstallPackageAsync (
+				nugetProject,
+				identity,
+				resolutionContext,
+				projectContext,
+				repositories,
+				null,
+				token
+			).ConfigureAwait (false);
+		}
+
+		static IEnumerable<SourceRepository> GetSourceRepositories (IEnumerable<PackageSourceInfo> sources)
+		{
+			var repositoryProvider = SourceRepositoryProviderFactory.CreateSourceRepositoryProvider ();
+			var allRepositories = repositoryProvider.GetRepositories ().ToList ();
+
+			var repositories = new List<SourceRepository> ();
+			foreach (PackageSourceInfo source in sources) {
+				var packageSource = new NuGet.Configuration.PackageSource (source.Source, source.Name);
+				SourceRepository matchedRepository = FindSourceRepository (packageSource, allRepositories);
+				if (matchedRepository != null) {
+					repositories.Add (matchedRepository);
+				} else {
+					var repository = repositoryProvider.CreateRepository (packageSource);
+					repositories.Add (repository);
+				}
+			}
+
+			return repositories;
+		}
+
+		static SourceRepository FindSourceRepository (NuGet.Configuration.PackageSource source, List<SourceRepository> repositories)
+		{
+			foreach (SourceRepository repository in repositories) {
+				if (repository.PackageSource.Equals (source)) {
+					return repository;
+				}
+			}
+			return null;
+		}
+
+		async Task<NuGetVersion> GetLatestPackageVersion (
+			IEnumerable<SourceRepository> repositories,
+			DependencyBehavior dependencyBehavior,
+			SourceCacheContext sourceCacheContext)
+		{
+			var latestVersionContext = new ResolutionContext (
+				dependencyBehavior,
+				message.AllowPrerelease,
+				false,
+				VersionConstraints.None,
+				new GatherCache (),
+				sourceCacheContext
+			);
+
+			ResolvedPackage resolvedPackage = await packageManager.GetLatestVersionAsync (
+				message.PackageId,
+				nugetProject,
+				latestVersionContext,
+				repositories,
+				new ProjectContextLogger (projectContext),
+				CancellationToken.None);
+
+			if (resolvedPackage?.LatestVersion == null) {
+				throw new InvalidOperationException (GettextCatalog.GetString ("Unable to find package '{0}", message.PackageId));
+			}
+
+			return resolvedPackage?.LatestVersion;
+		}
+
+		public async Task InstallPackageAsync (CancellationToken token)
+		{
+			using (var sourceCacheContext = new SourceCacheContext ()) {
+				var actions = await PreviewInstallPackage (token, sourceCacheContext);
+
+				NuGetPackageManager.SetDirectInstall (identity, projectContext);
+
+				await packageManager.ExecuteNuGetProjectActionsAsync (
+					nugetProject,
+					actions,
+					projectContext,
+					sourceCacheContext,
+					CancellationToken.None);
+
+				NuGetPackageManager.ClearDirectInstall (projectContext);
+			}
+		}
+	}
+}
--- src/MonoDevelop.PackageManagement.Extensions/MonoDevelop.PackageManagement.Protocol/ProjectMessageHandler.cs
+++ src/MonoDevelop.PackageManagement.Extensions/MonoDevelop.PackageManagement.Protocol/ProjectMessageHandler.cs
@@ -35,11 +35,6 @@ using MonoDevelop.Projects;
 using Newtonsoft.Json.Linq;
 using NuGet.PackageManagement;
 using NuGet.Packaging;
-using NuGet.Packaging.Core;
-using NuGet.ProjectManagement;
-using NuGet.Protocol.Core.Types;
-using NuGet.Resolver;
-using NuGet.Versioning;
 using StreamJsonRpc;
 
 namespace MonoDevelop.PackageManagement.Protocol
@@ -64,26 +59,10 @@ namespace MonoDevelop.PackageManagement.Protocol
 
 		async Task<IEnumerable<PackageReference>> GetInstalledPackages (DotNetProject project)
 		{
-			var solutionManager = GetSolutionManager (project);
-			var nugetProject = CreateNuGetProject (solutionManager, project);
+			var solutionManager = project.GetSolutionManager ();
+			var nugetProject =  project.CreateNuGetProject (solutionManager);
 
 			return await Task.Run (() => nugetProject.GetInstalledPackagesAsync (CancellationToken.None)).ConfigureAwait (false);
-		}
-
-		static IMonoDevelopSolutionManager GetSolutionManager (DotNetProject project)
-		{
-			return Runtime.RunInMainThread (() => {
-				return PackageManagementServices.Workspace.GetSolutionManager (project.ParentSolution);
-			}).Result;
-		}
-
-		static NuGetProject CreateNuGetProject (IMonoDevelopSolutionManager solutionManager, DotNetProject project)
-		{
-			if (solutionManager != null) {
-				return solutionManager.GetNuGetProject (new DotNetProjectProxy (project));
-			}
-
-			return new MonoDevelopNuGetProjectFactory ().CreateNuGetProject (project);
 		}
 
 		IEnumerable<PackageReferenceInfo> CreatePackageInformation (IEnumerable<PackageReference> packages)
@@ -120,32 +99,15 @@ namespace MonoDevelop.PackageManagement.Protocol
 			try {
 				var message = arg.ToObject<UninstallPackageParams> ();
 				var project = FindProject (message.ProjectFileName);
-				var actions = PreviewUninstallPackage (project, message).Result;
-				return new PackageActionList {
+				var handler = new UninstallPackageMessageHandler (project, message);
+				var actions = handler.PreviewUninstallPackage (CancellationToken.None).Result;
+					return new PackageActionList {
 					Actions = CreatePackageActionInformation (actions).ToArray ()
 				};
 			} catch (Exception ex) {
 				LoggingService.LogError ("OnPreviewUninstallPackage error: {0}", ex);
 				throw;
 			}
-		}
-
-		async Task<IEnumerable<NuGetProjectAction>> PreviewUninstallPackage (DotNetProject project, UninstallPackageParams message)
-		{
-			var solutionManager = GetSolutionManager (project);
-			var nugetProject = CreateNuGetProject (solutionManager, project);
-
-			var packageManager = new MonoDevelopNuGetPackageManager (solutionManager);
-			var uninstallationContext = new UninstallationContext (message.RemoveDependencies, message.Force);
-			var context = new NuGetProjectContext (solutionManager.Settings);
-
-			return await packageManager.PreviewUninstallPackageAsync (
-				nugetProject,
-				message.PackageId,
-				uninstallationContext,
-				context,
-				CancellationToken.None
-			).ConfigureAwait (false);
 		}
 
 		IEnumerable<PackageActionInfo> CreatePackageActionInformation (IEnumerable<NuGetProjectAction> actions)
@@ -168,37 +130,12 @@ namespace MonoDevelop.PackageManagement.Protocol
 			try {
 				var message = arg.ToObject<UninstallPackageParams> ();
 				var project = FindProject (message.ProjectFileName);
-				UninstallPackageAsync (project, message).Wait ();
+				var handler = new UninstallPackageMessageHandler (project, message);
+				handler.UninstallPackageAsync (CancellationToken.None).Wait ();
 			} catch (Exception ex) {
 				LoggingService.LogError ("OnUninstallPackage error: {0}", ex);
 				throw;
 			}
-		}
-
-		async Task UninstallPackageAsync (DotNetProject project, UninstallPackageParams message)
-		{
-			var solutionManager = GetSolutionManager (project);
-			var nugetProject = CreateNuGetProject (solutionManager, project);
-
-			var packageManager = new MonoDevelopNuGetPackageManager (solutionManager);
-			var uninstallationContext = new UninstallationContext (message.RemoveDependencies, message.Force);
-			var context = new NuGetProjectContext (solutionManager.Settings);
-
-			var actions = await packageManager.PreviewUninstallPackageAsync (
-				nugetProject,
-				message.PackageId,
-				uninstallationContext,
-				context,
-				CancellationToken.None
-			).ConfigureAwait (false);
-
-			await packageManager.ExecuteNuGetProjectActionsAsync (
-				nugetProject,
-				actions,
-				context,
-				NullSourceCacheContext.Instance,
-				CancellationToken.None
-			).ConfigureAwait (false);
 		}
 
 		[JsonRpcMethod (Methods.ProjectPreviewInstallPackage)]
@@ -207,7 +144,8 @@ namespace MonoDevelop.PackageManagement.Protocol
 			try {
 				var message = arg.ToObject<InstallPackageParams> ();
 				var project = FindProject (message.ProjectFileName);
-				var actions = PreviewInstallPackage (project, message).Result;
+				var handler = new InstallPackageMessageHandler (project, message);
+				var actions = handler.PreviewInstallPackage (CancellationToken.None).Result;
 				return new PackageActionList {
 					Actions = CreatePackageActionInformation (actions).ToArray ()
 				};
@@ -217,172 +155,17 @@ namespace MonoDevelop.PackageManagement.Protocol
 			}
 		}
 
-		async Task<IEnumerable<NuGetProjectAction>> PreviewInstallPackage (DotNetProject project, InstallPackageParams message)
-		{
-			var solutionManager = GetSolutionManager (project);
-			var nugetProject = CreateNuGetProject (solutionManager, project);
-
-			var packageManager = new MonoDevelopNuGetPackageManager (solutionManager);
-			var repositories = GetSourceRepositories (message.PackageSources);
-
-			var context = new NuGetProjectContext (solutionManager.Settings);
-			var dependencyBehavior = (DependencyBehavior)Enum.Parse (typeof (DependencyBehavior), message.DependencyBehavior);
-
-			using (var sourceCacheContext = new SourceCacheContext ()) {
-
-				NuGetVersion version = null;
-				if (string.IsNullOrEmpty (message.PackageVersion)) {
-					version = await GetLatestPackageVersion (message, nugetProject, packageManager, repositories, context, dependencyBehavior, sourceCacheContext);
-				} else {
-					version = NuGetVersion.Parse (message.PackageVersion);
-				}
-
-				var resolutionContext = new ResolutionContext (
-					dependencyBehavior,
-					message.AllowPrerelease,
-					true,
-					VersionConstraints.None,
-					new GatherCache (),
-					sourceCacheContext
-				);
-
-				var identity = new PackageIdentity (message.PackageId, version);
-				return await packageManager.PreviewInstallPackageAsync (
-					nugetProject,
-					identity,
-					resolutionContext,
-					context,
-					repositories,
-					null,
-					CancellationToken.None
-				).ConfigureAwait (false);
-			}
-		}
-
-		static IEnumerable<SourceRepository> GetSourceRepositories (IEnumerable<PackageSourceInfo> sources)
-		{
-			var repositoryProvider = SourceRepositoryProviderFactory.CreateSourceRepositoryProvider ();
-			var allRepositories = repositoryProvider.GetRepositories ().ToList ();
-
-			var repositories = new List<SourceRepository> ();
-			foreach (PackageSourceInfo source in sources) {
-				var packageSource = new NuGet.Configuration.PackageSource (source.Source, source.Name);
-				SourceRepository matchedRepository = FindSourceRepository (packageSource, allRepositories);
-				if (matchedRepository != null) {
-					repositories.Add (matchedRepository);
-				} else {
-					var repository = repositoryProvider.CreateRepository (packageSource);
-					repositories.Add (repository);
-				}
-			}
-
-			return repositories;
-		}
-
-		static SourceRepository FindSourceRepository (NuGet.Configuration.PackageSource source, List<SourceRepository> repositories)
-		{
-			foreach (SourceRepository repository in repositories) {
-				if (repository.PackageSource.Equals (source)) {
-					return repository;
-				}
-			}
-			return null;
-		}
-
-		static async Task<NuGetVersion> GetLatestPackageVersion (
-			InstallPackageParams message,
-			NuGetProject nugetProject,
-			MonoDevelopNuGetPackageManager packageManager,
-			IEnumerable<SourceRepository> repositories,
-			NuGetProjectContext context,
-			DependencyBehavior dependencyBehavior,
-			SourceCacheContext sourceCacheContext)
-		{
-			var latestVersionContext = new ResolutionContext (
-				dependencyBehavior,
-				message.AllowPrerelease,
-				false,
-				VersionConstraints.None,
-				new GatherCache (),
-				sourceCacheContext
-			);
-
-			ResolvedPackage resolvedPackage = await packageManager.GetLatestVersionAsync (
-				message.PackageId,
-				nugetProject,
-				latestVersionContext,
-				repositories,
-				new ProjectContextLogger (context),
-				CancellationToken.None);
-
-			if (resolvedPackage?.LatestVersion == null) {
-				throw new InvalidOperationException (GettextCatalog.GetString ("Unable to find package '{0}", message.PackageId));
-			}
-
-			return resolvedPackage?.LatestVersion;
-		}
-
 		[JsonRpcMethod (Methods.ProjectInstallPackage)]
 		public void OnInstallPackage (JToken arg)
 		{
 			try {
 				var message = arg.ToObject<InstallPackageParams> ();
 				var project = FindProject (message.ProjectFileName);
-				InstallPackageAsync (project, message).Wait ();
+				var handler = new InstallPackageMessageHandler (project, message);
+				handler.InstallPackageAsync (CancellationToken.None).Wait ();
 			} catch (Exception ex) {
 				LoggingService.LogError ("OnUninstallPackage error: {0}", ex);
 				throw;
-			}
-		}
-
-		async Task InstallPackageAsync (DotNetProject project, InstallPackageParams message)
-		{
-			var solutionManager = GetSolutionManager (project);
-			var nugetProject = CreateNuGetProject (solutionManager, project);
-
-			var packageManager = new MonoDevelopNuGetPackageManager (solutionManager);
-			var repositories = GetSourceRepositories (message.PackageSources);
-
-			var context = new NuGetProjectContext (solutionManager.Settings);
-			var dependencyBehavior = (DependencyBehavior)Enum.Parse (typeof (DependencyBehavior), message.DependencyBehavior);
-
-			using (var sourceCacheContext = new SourceCacheContext ()) {
-
-				NuGetVersion version = null;
-				if (string.IsNullOrEmpty (message.PackageVersion)) {
-					version = await GetLatestPackageVersion (message, nugetProject, packageManager, repositories, context, dependencyBehavior, sourceCacheContext);
-				} else {
-					version = NuGetVersion.Parse (message.PackageVersion);
-				}
-
-				var resolutionContext = new ResolutionContext (
-					dependencyBehavior,
-					message.AllowPrerelease,
-					true,
-					VersionConstraints.None,
-					new GatherCache (),
-					sourceCacheContext
-				);
-
-				var identity = new PackageIdentity (message.PackageId, version);
-				var actions = await packageManager.PreviewInstallPackageAsync (
-					nugetProject,
-					identity,
-					resolutionContext,
-					context,
-					repositories,
-					null,
-					CancellationToken.None
-				).ConfigureAwait (false);
-
-				NuGetPackageManager.SetDirectInstall (identity, context);
-				await packageManager.ExecuteNuGetProjectActionsAsync (
-					nugetProject,
-					actions,
-					context,
-					sourceCacheContext,
-					CancellationToken.None);
-				NuGetPackageManager.ClearDirectInstall (context);
 			}
 		}
 	}
