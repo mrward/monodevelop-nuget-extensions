a01a42d51a2ef78403ece303e185f5c43d132314
Matt Ward <ward.matt@gmail.com>
2019-02-09 17:16:40 +0000

    Add Register-TabExpansion function
    Tab expansion is not supported but defining the function allows
    Microsoft.EntityFrameworkCore.Tools to be installed and have its
    init.ps1 run.

--- src/MonoDevelop.PackageManagement.PowerShell.ConsoleHost/Modules/NuGet/nuget.psm1
+++ src/MonoDevelop.PackageManagement.PowerShell.ConsoleHost/Modules/NuGet/nuget.psm1
@@ -1,6 +1,41 @@
 ﻿# make sure we stop on exceptions
 $ErrorActionPreference = "Stop"
 
+# This object reprents the result value for tab expansion functions when no result is returned.
+# This is so that we can distinguish it from $null, which has different semantics
+$NoResultValue = New-Object PSObject -Property @{ NoResult = $true }
+
+# Hashtable that stores tab expansion definitions
+$TabExpansionCommands = New-Object 'System.Collections.Hashtable' -ArgumentList @([System.StringComparer]::InvariantCultureIgnoreCase)
+
+function Register-TabExpansion {
+<#
+.SYNOPSIS
+    Registers a tab expansion for the parameters of the specified command.
+.DESCRIPTION
+    Registers a tab expansion for the parameters of the specified command.
+.PARAMETER Name
+    Name of the command the expansion is for.
+.EXAMPLE
+    PS> Register-TabExpansion 'Set-Color', @{'color' = {'blue', 'green', 'red'}}
+         This adds a tab expansion to the Set-Color command. Set-Color contains a single
+         parameter, color, with three possible expansion values.
+#>
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true)]
+        [string]$Name,
+        [parameter(Mandatory = $true)]
+        $Definition
+    )
+
+    # transfer $definition data into a new hashtable that compare values using InvariantCultureIgnoreCase
+    $normalizedDefinition = New-Object 'System.Collections.Hashtable' -ArgumentList @([System.StringComparer]::InvariantCultureIgnoreCase)
+    $definition.GetEnumerator() | % { $normalizedDefinition[$_.Name] = $_.Value }
+
+    $TabExpansionCommands[$Name] = $normalizedDefinition
+}
+
 
 function Format-ProjectName {
     param(
