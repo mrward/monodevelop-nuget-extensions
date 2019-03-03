3cdeb68ab97588b497c28e4bafee11e98132320e
Matt Ward <ward.matt@gmail.com>
2019-03-03 14:27:29 +0000

    Update generate mpack script
    Ensure PowerShell console host files are published and moved to
    the correct folder before generating the .mpack

--- gen-mpack.sh
+++ gen-mpack.sh
@@ -1,1 +1,3 @@
+./powershell-host-publish.sh
+cp -r bin/PowerShellConsoleHost/publish/* bin/PowerShellConsoleHost/
 mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack bin/MonoDevelop.PackageManagement.Extensions.dll
