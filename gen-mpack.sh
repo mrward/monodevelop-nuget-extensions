9d87bd721b84084c3b95ecb2b80e7262dbee35da
Matt Ward <ward.matt@gmail.com>
2022-05-29 12:03:03 +0100

    Generate .mpack with VS Mac 8.10

--- gen-mpack.sh
+++ gen-mpack.sh
@@ -1,1 +1,1 @@
-mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack bin/MonoDevelop.PackageManagement.Extensions.dll
+mono /Applications/Visual\ Studio\ 2019.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack bin/MonoDevelop.PackageManagement.Extensions.dll
