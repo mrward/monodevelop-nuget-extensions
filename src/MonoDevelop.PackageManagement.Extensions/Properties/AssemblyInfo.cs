f61934f06b47e6f10dc0cd86cc202f79a3d1b547
Matt Ward <ward.matt@gmail.com>
2022-12-03 12:55:58 +0000

    Update addin version to 0.34

--- src/MonoDevelop.PackageManagement.Extensions/Properties/AssemblyInfo.cs
+++ src/MonoDevelop.PackageManagement.Extensions/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Runtime.Versioning;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.33")]
+[assembly: AssemblyVersion ("0.34")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
